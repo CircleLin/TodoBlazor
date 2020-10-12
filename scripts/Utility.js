e6d1ae8b348e177bedb678e4acd3acc4514eda5f
CircleLin <wayne7428@gmail.com>
2020-10-12 13:53:22 +0000

    Deploying to gh-pages from  @ c9fc4ca7a97795613ef4c5f3968415d0724b4a67 🚀

---
+++ scripts/Utility.js
@@ -0,0 +1,17 @@
+﻿function SweetConfirm(title, msg) {
+    return new Promise((resolve) => {
+        Swal.fire({
+            title: title,
+            text: msg,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: '刪除'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                result.value ? resolve(true) : resolve(false);
+            }
+        });
+    });
+}
